941d0a95378a96557248cfd7803b225023bbd7cc
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2024-07-28 21:06:13 +0300

    gen-random-prefixes.py: fix

--- gen-random-prefixes.py
+++ gen-random-prefixes.py
@@ -10,6 +10,8 @@ b = int(sys.argv[4])
 random.seed(s)
 while b > 0:
     for c in itertools.product('0123456789', repeat=n):
+        if b <= 0:
+            break
         if random.random() < p:
             print(''.join(c))
             b -= len(c)
